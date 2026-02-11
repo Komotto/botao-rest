08323a7b2b1235474233bf8bd22284cc8f2b0067
Komotto <58824478+Komotto@users.noreply.github.com>
2026-02-11 17:18:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname)));
+app.use(cors()); // libera para qualquer origem
+
+app.get('/api/alo', (req, res) => {
+  res.json({ mensagem: "Alô mundo" });
+});
 
 app.listen(8080, () => {
-  console.log('Frontend rodando na porta 8080');
+  console.log("API rodando na porta 8080");
 });
