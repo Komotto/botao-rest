58cfc939778160b0a94bd7f1056dab799b95ba20
Komotto <58824478+Komotto@users.noreply.github.com>
2026-02-12 08:57:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send("API funcionando 🚀");
+  res.json({ status: "API funcionando 🚀" });
 });
 
 app.get('/api/alo', (req, res) => {
